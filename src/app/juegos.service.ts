ac7884f19ee81b7a2540cb742f9a27987c17c000
Juan Rueda <juanandresrueda1@gmail.com>
2019-02-21 23:18:16 -0300

    Cambios visuales

--- src/app/juegos.service.ts
+++ src/app/juegos.service.ts
@@ -9,7 +9,7 @@ export class JuegosService {
   private base_url = 'http://localhost:3000';
 
   getJuegos():Observable<any>{
-    return this.http.get(this.base_url+'/juegos');
+    return this.http.get(this.base_url+'/juegosActivos');
   }
 
 }
